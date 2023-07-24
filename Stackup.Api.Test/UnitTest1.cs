ce2ab4c7c401b767a1e1d3ea8457767301dc2a6b
scvinod <s.c.vinod@gmail.com>
2023-07-24 21:07:34 +0530

    Initial commit

---
+++ Stackup.Api.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Stackup.Api.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
