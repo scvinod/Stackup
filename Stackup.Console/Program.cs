4a2039876d8327571ba83a70d2d9c3d0a61eee89
VinodChengalvarayan <47827727+VinodChengalvarayan@users.noreply.github.com>
2023-08-10 11:52:05 +0530

    Testing

---
+++ Stackup.Console/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+await DelayWithCallback(10000, () =>
+{
+   Console.WriteLine("Completed");
+});
+Console.WriteLine("Done");
+
+
+static async Task DelayWithCallback(int millisecondsDelay, Action callback)
+{
+    await Task.Delay(millisecondsDelay);
+    callback?.Invoke();
+}
