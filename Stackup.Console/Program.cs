e73202bcb7b8758fd5978673b9af8a0b82380558
VinodChengalvarayan <47827727+VinodChengalvarayan@users.noreply.github.com>
2023-08-17 21:52:47 +0530

    Update Program.cs

--- Stackup.Console/Program.cs
+++ Stackup.Console/Program.cs
@@ -1,14 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-await DelayWithCallback(10000, () =>
-{
-   Console.WriteLine("Completed");
-});
-Console.WriteLine("Done");
+﻿using System;
+using System.Numerics;
 
-
-static async Task DelayWithCallback(int millisecondsDelay, Action callback)
+public class Program
 {
-    await Task.Delay(millisecondsDelay);
-    callback?.Invoke();
+    private const int ERROR_SUCCESS = 0;
+    private const int ERROR_BAD_ARGUMENTS = 0xA0;
+    private const int ERROR_ARITHMETIC_OVERFLOW = 0x216;
+    private const int ERROR_INVALID_COMMAND_LINE = 0x667;
+
+    public async static Task<int> Main()
+    {
+        Console.WriteLine("Hello, World!");
+        await DelayWithCallback(10000, () =>
+        {
+            Console.WriteLine("Completed");
+        });
+        Console.WriteLine("Done");
+        return ERROR_SUCCESS;
+    }
+
+    static async Task DelayWithCallback(int millisecondsDelay, Action callback)
+    {
+        await Task.Delay(millisecondsDelay);
+        callback?.Invoke();
+    }
 }
